a7d8cb47dc4da177b3c7f7964523b5ddb5f677ed
Deshan Dewinda <dddeshandewinda@gmail.com>
2023-07-31 08:08:03 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+
+public static void main(String[] args) {
+    
+    double Myaccount = 123456.7890;
+    System.out.printf("my balance = $%,.2f",Myaccount);
+    
+}
+}
---
+++ Demo3.java
@@ -0,0 +1,18 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        
+        int Telephnum_1=123;
+        int Telephnum_2=456;
+        int Telephnum_3=7890;
+
+        String a= "\033[34;1m";
+        String b= "\033[34;0";
+
+        
+        System.out.printf("(%s%s%s)%s-%s",a,Telephnum_1,b,Telephnum_2,Telephnum_3);
+
+
+
+    }
+    
+}
